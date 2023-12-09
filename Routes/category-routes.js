8bbc6276fbe5dbf5caaf3d16c612374f4d7160e6
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-12-09 00:33:21 +0000

    create category details route

---
+++ Routes/category-routes.js
@@ -0,0 +1,4 @@
+const { Express } = require("express");
+const router = express.router();
+
+router.get("/", () => {});
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const url = "mongodb://127.0.0.1:27017";
 let db_name = "easitoryDB";
 
@@ -36,6 +36,28 @@ app.get("/categories", (req, res) => {
       categories.push(category);
     })
     .then(() => {
+      console.log(categories);
       res.status(200).json({ categories: categories });
     });
+});
+
+// get category details
+app.get("/categories/:id", (req, res) => {
+  let id = req.params.id;
+  console.log("id", id);
+
+  if (!ObjectId.isValid) {
+    db.collection("category")
+      .findOne({ _id: new ObjectId(id) })
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  } else {
+    res.status(500).json({ msg: "invalid ID" });
+  }
 });
