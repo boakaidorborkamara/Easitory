852c185e8a8cb5138005e0df0be3bcea51467e10
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2024-01-05 15:58:01 +0000

    add new item to the database

--- Controller/itemController.js
+++ Controller/itemController.js
@@ -56,18 +56,61 @@ const displayNewItemForm = (req, res) => {
   res.render("pages/add-item");
 };
 
-const addItem = (req, res) => {
-  let new_item = req.body;
-  console.log("new item", new_item);
-  res.status(200).json({ msg: "received" });
-  return;
-  db.collection("items")
-    .insertOne(new_item)
-    .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
-    });
-};
+const addItem = [
+  (req, res, next) => {
+    const fs = require("fs");
+    const path = require("path");
+    const { Buffer } = require("buffer");
+
+    let new_item = req.body;
+    let base64_data = new_item.image.file_data;
+
+    // get data string from base64_data text by splitting and removing un-needed strings
+    base64_data = base64_data.split(";");
+    base64_data = base64_data[1];
+    base64_data = base64_data.split(",");
+    base64_data = base64_data[1];
+
+    // convert base64_string to binary data
+    let buff = Buffer.from(base64_data, "base64");
+
+    // create unique filename for each file by including the date the file was uploaded
+    let file_path = "./Public/images/uploads/";
+    let date_created = Date.now();
+    let new_item_image = new_item.image.file_details.name;
+
+    // remove white spaces from file name
+    new_item_image = new_item_image.split(" ");
+    new_item_image = new_item_image.join("-");
+
+    let image_path = `/images/uploads/${date_created}-${new_item_image}`;
+    let file_name = `${file_path}${date_created}-${new_item_image}`;
+
+    // create a new file from binary data
+    fs.writeFileSync(file_name, buff);
+    console.log(
+      "Base64 image data converted to file: stack-abuse-logo-out.png"
+    );
+
+    // modify request body
+    req.body.image = image_path;
+
+    // call next middleware to upload data to db after creating the file
+    next();
+  },
+  (req, res) => {
+    let new_item = req.body;
+    console.log(req.body);
+    // res.status(200).json({ msg: "received" });
+
+    db.collection("items")
+      .insertOne(new_item)
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      });
+  },
+];
 
 const editItem = (req, res) => {
   let query = { id: req.params.id };
